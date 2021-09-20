9fe4394f15cd5b7cbcf2df3fdad51b1191bf4dc2
 Leͥgeͣnͫd <longthinhiphone@gmail.com>
2021-09-20 16:43:23 +0700

    Update

--- PACProfile/PACProfile.js
+++ PACProfile/PACProfile.js
@@ -1,6 +1,7 @@
 var DIRECT = "PROXY 127.0.0.1:6152; DIRECT";
 var PROXY = "PROXY 127.0.0.1:6153";
 var blacklist = {
+    "su.itunes.apple.com":1,
     "gdmf.apple.com":1,
 };
 function FindProxyForURL(url, host) {
